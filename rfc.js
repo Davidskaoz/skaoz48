d4344ce8644493cbd2f531a0dc1dfdd94ebe0d53
David <davidg@garagecoders.net>
2018-10-09 12:31:33 -0500

    Mejora en los comentarios de Codigo

--- rfc.js
+++ rfc.js
@@ -8,75 +8,68 @@ var request = require('request');
 var driver =  new webdriver.Builder()
 .forBrowser('firefox')
 .build();
+// accedemos al pagina que se desea scrapear
 driver.get('https://agsc.siat.sat.gob.mx/PTSC/ValidaRFC/index.jsf?ispublic=1');
+
+//buacamos la imagen del captcha
 img = driver.findElement(By.id('captchaSession')).getAttribute('src');
 
 img.then(function(text){
 
+    // se obtieene el codigo en base64 de la imagen
     var image = text.split("data:image/png;base64,");
 
 
+    // se envia la imagen a un tercero para su resolucion en este caso usamos el servicio de anticaptcha
     resolve_image(image[1] ,function(idResolv){
 
-        
+                    //una vez obtenido el captcha resulto arracamos de nuevo el scrapeo 
                    resolve_captcha(idResolv,function(result){
         
-                                                console.log(result);
+                            console.log(result);
+                            // se escribe el la resolucion del captcha en el input 
                             driver.findElement(By.id("formMain:captchaInput")).sendKeys(result);
                             clearTimeout(resolucion);
                             driver.findElement(By.id("formMain:j_idt57")).click();
         
                             driver.wait(until.elementLocated(By.id('formMain:valRFC')), 2000).then(function(elm) {
                                 
-                               
+                             /* introducimos el RFC  a validar*/ 
                              driver.findElement(By.id("formMain:valRFC")).sendKeys('CAVJ911003QY1');
                              driver.findElement(By.id('formMain:consulta')).click();
 
-
+                                     /*  damos clieck en el boton validar*/ 
                                     driver.wait(until.elementLocated(By.id('formMain:terminar')), 2000).then(function(elm) {
 
 
+                                      /*  recogemos la respuesta de la peticion */
                                        msg = driver.findElement(By.className('ui-messages-info ui-corner-all')).getText();
 
                                        msg.then(function(text){
 
-
                                          console.log(text);
+
+                                         /*  cerramos la conexion */
 
                                          driver.quit();
 
                                        });
-
-
-
-
 
                                     }).catch(function(ex) {
                                         console.log('ex')
                                         driver.quit();
                                     });
                                     
-                                
-                                 
-           
-           
-                              
+
                             }).catch(function(ex) {
                                 console.log('ex')
                                 driver.quit();
                             });
-        
-                            
-        
-                          
+ 
         
                      });
         
-         
-        
              });
-        
-
 
 });
 
@@ -102,7 +95,6 @@ resolucion =  setInterval(function(){
         if(body.status === 'processing')
         {
 
-            
             console.log('espera');
         
         }else{
